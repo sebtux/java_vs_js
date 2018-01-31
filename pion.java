231f10c295a190cd86d3e9ee4c08903f1cd17d22
sebtux <sebtux@gmail.com>
2018-01-31 15:39:15 +0100

    Add files via upload

---
+++ pion.java
@@ -0,0 +1,20 @@
+
+public class pion {
+	String nom;
+	String [] couleur= {"rouge", "bleu", "jaune", "vert"};
+
+	public pion() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public pion arg(String nom, String[] couleur) {
+		nom=this.nom;
+		couleur=this.couleur;
+		return null;
+	}
+
+		private void deplacement() {
+		/*faire un svitchcase pour les deplacement en fontion du nom*/
+
+	}
+}
