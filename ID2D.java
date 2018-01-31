6bc5a73d1ea94609e462fcb80dddb83f8cb00f19
sebtux <sebtux@gmail.com>
2018-01-31 16:39:47 +0100

    Add files via upload

---
+++ ID2D.java
@@ -0,0 +1,14 @@
+
+public class ID2D {
+	char[] n= {'l', 'e', 'm'};
+	int[] nb;
+
+	public ID2D() {
+		// TODO Auto-generated constructor stub
+	}
+	public ID2D arg(char n, int nb) {
+		return null;
+		// TODO Auto-generated constructor stub
+	}
+
+}
