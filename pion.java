4135641b93686e3b3825bdcad9ccee36515c4d85
sebtux <sebtux@gmail.com>
2018-02-08 14:23:59 +0100

    pret à telecharger à ce jour

--- pion.java
+++ pion.java
@@ -3,10 +3,10 @@ public class pion {
 	String nom;
 	String [] couleur= {"rouge", "bleu", "jaune", "vert"};
 	ID2D id=new ID2D();
-	public pion() {
+	public pion() {/*/construteur 1/*/
 		// TODO Auto-generated constructor stub
 	}
-	
+	/*/construteur 2/*/
 	public pion arg(String nom, String[] couleur, ID2D id) {
 		nom=this.nom;
 		couleur=this.couleur;
@@ -16,6 +16,6 @@ public class pion {
 
 		private void deplacement() {
 		/*faire un svitchcase pour les deplacement en fontion du nom*/
-		/*Ã§a fait 3jours que j'attend ta sructure de controle ^_^ */
+			/*ça fait 3jours que j'attend ta sructure de controle ^_^ */
 	}
 }
